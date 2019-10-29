e8a0c5680da86a2b70dcde669d45973540ed04f7
Charlie <scheersound@gmail.com>
2019-10-29 15:34:23 -0700

    added interactive elements to table view, changed some design layout

--- AllNotifictions/AllNotifictions/View/ViewControllers/LoginListViewController.swift
+++ AllNotifictions/AllNotifictions/View/ViewControllers/LoginListViewController.swift
@@ -9,24 +9,42 @@
 import UIKit
 
 class LoginListViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func skipWasTapped(_ sender: Any) {
+        
+    }
+    
 }
 
 extension LoginListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let tableCell = tableView.dequeueReusableCell(withIdentifier: constants.cellIdentifier, for: indexPath) as! LoginCell
         
+        tableCell.serviceLogoImage.image = UIImage(named: "not-found")
+        tableCell.loggedInCheckBoxImage.alpha = 0
+        tableCell.serviceTitleLabel.text = "WordPress"
+        
         return tableCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! LoginCell
+        
+        cell.loggedInCheckBoxImage.alpha = 1
     }
     
     
