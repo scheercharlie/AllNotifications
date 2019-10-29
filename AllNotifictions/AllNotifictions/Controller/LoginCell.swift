032acd1a74bfb5dcefb9413ef09418660ed51151
Charlie <scheersound@gmail.com>
2019-10-29 14:59:52 -0700

    Set custom cell for LoginCell

---
+++ AllNotifictions/AllNotifictions/Controller/LoginCell.swift
@@ -0,0 +1,13 @@
+//
+//  LoginCell.swift
+//  AllNotifictions
+//
+//  Created by Charlie Scheer on 10/29/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+class LoginCell: UITableViewCell {
+    
+}
