28c78504192b612d72d4e87f86d890f6bfc9491b
Charlie <scheersound@gmail.com>
2019-10-29 14:34:30 -0700

    Added new development project

---
+++ AllNotifictions/AllNotifictions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllNotifictions
+//
+//  Created by Charlie Scheer on 10/29/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
