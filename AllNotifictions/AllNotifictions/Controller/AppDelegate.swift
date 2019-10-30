2f64c3e0f3f7031eed2c7bbb5470054561d697a0
Charlie <scheersound@gmail.com>
2019-10-29 22:41:29 -0700

    Refactored the creation of the host services

--- AllNotifictions/AllNotifictions/Controller/AppDelegate.swift
+++ AllNotifictions/AllNotifictions/Controller/AppDelegate.swift
@@ -11,52 +11,66 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let context = persistentContainer.viewContext
+        //Prepare a fetched results controller for NotificationHosts
+        //If all of the Host options do not exist create them.
+        let fetchedResultsController = prepareFetchedResultsController()
+        if let fetchedObjects = fetchedResultsController.fetchedObjects, fetchedObjects.count != HostType.SocialService.allCases.count {
+            createHostTypeObjects()
+            
+            if persistentContainer.viewContext.hasChanges {
+                do {
+                    try persistentContainer.viewContext.save()
+                } catch {
+                    print("Save Failed")
+                }
+            }
+        }
+        
 
+        return true
+    }
+    
+    //Prepares a fetched results controller to make sure the app contains the Host Objects
+    fileprivate func prepareFetchedResultsController() -> NSFetchedResultsController<NotificationHost> {
         let request: NSFetchRequest<NotificationHost> = NotificationHost.fetchRequest()
         let sortDescriptor = NSSortDescriptor.init(key: "objectID", ascending: true)
         request.sortDescriptors = [sortDescriptor]
         
         var frc = NSFetchedResultsController<NotificationHost>()
         
-        frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: "service")
+        frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: "service")
         
         do {
             try frc.performFetch()
         } catch {
+            //TO DO: Handle the fatelError better
             fatalError()
         }
         
-        if let fetchedObjects = frc.fetchedObjects, fetchedObjects.count < 1 {
-            print("less than 1")
-            for service in HostType.SocialService.allCases {
-                switch service {
-                case .github:
-                    let github = NotificationHost(context: context)
-                    github.hostType?.type = service
-                    github.isLoggedIn = false
-                case .wordpress:
-                    let wordpress = NotificationHost(context: context)
-                    wordpress.hostType?.type = service
-                    wordpress.isLoggedIn = false
-                case .slack:
-                    let slack = NotificationHost(context: context)
-                    slack.hostType?.type = service
-                    slack.isLoggedIn = false
-                }
+        return frc
+    }
+    
+    //Create new host objects
+    fileprivate func createHostTypeObjects() {
+        for service in HostType.SocialService.allCases {
+            switch service {
+            case .github:
+                let github = NotificationHost(context: persistentContainer.viewContext)
+                github.hostType?.type = service
+                github.isLoggedIn = false
+            case .wordpress:
+                let wordpress = NotificationHost(context: persistentContainer.viewContext)
+                wordpress.hostType?.type = service
+                wordpress.isLoggedIn = false
+            case .slack:
+                let slack = NotificationHost(context: persistentContainer.viewContext)
+                slack.hostType?.type = service
+                slack.isLoggedIn = false
             }
-        } else {
-            print("more than 1")
         }
-        print(context.hasChanges)
-        
-        return true
     }
     
     // MARK: UISceneSession Lifecycle
--- AllNotifictions/AllNotifictions/Model/Classes/HostType.swift
+++ AllNotifictions/AllNotifictions/Model/Classes/HostType.swift
@@ -8,11 +8,12 @@
 
 import Foundation
 
+//A class of available Notification Host Services
 public class HostType: NSObject, NSSecureCoding {
     public static var supportsSecureCoding: Bool = true
     var type: SocialService!
 
-    
+    //Required functions for using with Transformable core data type
     public func encode(with coder: NSCoder) {
         coder.encode(type, forKey: "type")
     }
@@ -20,12 +21,8 @@ public class HostType: NSObject, NSSecureCoding {
     public required init?(coder: NSCoder) {
         coder.decodeObject(forKey: "type")
     }
-
     
-    init(type: SocialService) {
-        self.type = type
-    }
-    
+    //Defined available services
     enum SocialService: Int, CaseIterable, Codable {
         case github = 1, wordpress, slack
     }
