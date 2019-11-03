d9a80010e94c5d650f2ef82ba4ba473892004ab4
Charlie <scheersound@gmail.com>
2019-11-02 20:05:32 -0700

    Created WordPress API class

---
+++ AllNotifictions/AllNotifictions/Model/Classes/WordpressAPIClient.swift
@@ -0,0 +1,13 @@
+//
+//  GithubAPIClient.swift
+//  AllNotifictions
+//
+//  Created by Charlie Scheer on 11/2/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import Foundation
+
+class WordpressAPIClient: APIClient {
+    
+}
